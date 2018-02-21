2bac31df4fee7e68e4b8368993705905656df296
Yura-Korbut <korbuty@mail.ru>
2018-02-22 01:50:20 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  while (str.length) {
+    let count = 0;
+
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            let brackets = bracketsConfig[i].join('');
+            let foundedPos = str.indexOf(brackets);
+    
+            if (foundedPos != -1) {
+                str = str.slice(0, foundedPos) + str.slice(foundedPos + 2);
+                count += 1;
+            }
+            else {
+                continue;
+            }
+        }
+        if (count == 0) break;
+    }
+    if (str.length)  return false
+     else return true;
+  }
